3d2f95262a9c19d1eb871034d127d36a39016916
Evan Phoenix <evan@phx.io>
2014-08-08 10:21:10 -0700

    Cleanup using consul_kv_cache

--- consul_routing.go
+++ consul_routing.go
@@ -1,25 +1,19 @@
 package mailbox
 
 import (
-	"bytes"
 	"crypto/sha1"
 	"encoding/hex"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
+
 	"net/http"
-	"strconv"
-	"strings"
 	"sync"
-	"sync/atomic"
-	"time"
-	"unsafe"
 
 	"github.com/vektra/consul_kv_cache/cache"
 )
+
+type cachedPusher struct {
+	clock  cache.ClockValue
+	pusher Pusher
+}
 
 type consulRoutingTable struct {
 	selfId []byte
@@ -34,7 +28,7 @@ type consulRoutingTable struct {
 
 	connections map[string]*consulPusher
 
-	cache *map[string]Pusher
+	cache map[string]*cachedPusher
 }
 
 func NewConsulRoutingTable(id string) (*consulRoutingTable, error) {
@@ -42,98 +36,38 @@ func NewConsulRoutingTable(id string) (*consulRoutingTable, error) {
 	h.Write([]byte(id))
 	k := hex.EncodeToString(h.Sum(nil))
 
-	tbl := make(map[string]Pusher)
+	consul := cache.NewConsulKVCache("mailbox-routing")
+
+	go consul.BackgroundUpdate()
 
 	ct := &consulRoutingTable{
 		selfId:      []byte(id),
 		key:         k,
 		names:       nil,
 		local:       make(MemRouteTable),
-		consul:      cache.NewConsulKVCache("mailbox-routing"),
-		cache:       &tbl,
+		consul:      consul,
 		connections: make(map[string]*consulPusher),
+		cache:       make(map[string]*cachedPusher),
 	}
 
 	return ct, nil
 }
 
-var bTrue = []byte("true\n")
-
-func setConsulKV(key string, value []byte) error {
-	url := "http://localhost:8500/v1/kv/" + key
-
-	body := bytes.NewReader(value)
-
-	req, err := http.NewRequest("PUT", url, body)
-	if err != nil {
-		return err
-	}
-
-	req.ContentLength = int64(len(value))
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-
-	defer resp.Body.Close()
-
-	data, err := ioutil.ReadAll(resp.Body)
-
-	if !bytes.Equal(data, bTrue) {
-		errors.New("Consul returned an error setting the value")
-	}
-
-	return nil
-}
-
-func delConsulKV(key string, rec bool) error {
-	url := "http://localhost:8500/v1/kv/" + key
-
-	if rec {
-		url += "?recurse"
-	}
-
-	req, err := http.NewRequest("DELETE", url, nil)
-	if err != nil {
-		return err
-	}
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return err
-	}
-
-	resp.Body.Close()
-
-	return nil
+func (ct *consulRoutingTable) Close() {
+	ct.consul.Close()
 }
 
 func (ct *consulRoutingTable) Set(name string, p Pusher) error {
-	ct.local.Set(name, p)
-
-	url := "http://localhost:8500/v1/kv/mailbox-routing/" + name + "/" + ct.key
-
-	body := bytes.NewReader(ct.selfId)
-
-	req, err := http.NewRequest("PUT", url, body)
+	err := ct.local.Set(name, p)
 	if err != nil {
-		log.Fatalf("Error creating http req: %s, %#v\n", err, req)
+		return err
 	}
 
-	req.ContentLength = int64(len(ct.selfId))
+	key := name + "/" + ct.key
 
-	resp, err := http.DefaultClient.Do(req)
+	err = ct.consul.Set(key, ct.selfId)
 	if err != nil {
-		log.Fatalf("Error sending PUT req: %#v\n", resp)
-	}
-
-	defer resp.Body.Close()
-
-	data, err := ioutil.ReadAll(resp.Body)
-
-	if !bytes.Equal(data, bTrue) {
-		log.Fatalf("Consul returned an error setting the value")
+		return err
 	}
 
 	ct.names = append(ct.names, name)
@@ -158,25 +92,19 @@ func (ct *consulRoutingTable) Get(name string) (Pusher, bool) {
 		return lp, true
 	}
 
-	url := "http://localhost:8500/v1/kv/mailbox-routing/" + name + "?recurse"
-
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, false
-	}
-
-	defer resp.Body.Close()
-
-	var values []consulValue
-
-	dec := json.NewDecoder(resp.Body)
-	err = dec.Decode(&values)
-	if err != nil && err != io.EOF {
-		return nil, false
-	}
+	values, clock := ct.consul.GetPrefix(name)
 
 	if len(values) == 0 {
 		return nil, false
+	}
+
+	if cp, ok := ct.cache[name]; ok {
+		if cp.clock >= clock {
+			debugf("using cached value to: %d >= %d\n", cp.clock, clock)
+			return cp.pusher, true
+		} else {
+			debugf("ignoring cached value to: %d < %d\n", cp.clock, clock)
+		}
 	}
 
 	if len(values) == 1 {
@@ -191,6 +119,8 @@ func (ct *consulRoutingTable) Get(name string) (Pusher, bool) {
 		ct.connections[id] = cp
 
 		cp.Connect()
+
+		ct.cache[name] = &cachedPusher{clock, cp}
 
 		return cp, true
 	}
@@ -213,114 +143,9 @@ func (ct *consulRoutingTable) Get(name string) (Pusher, bool) {
 		}
 	}
 
+	ct.cache[name] = &cachedPusher{clock, mp}
+
 	return mp, true
-}
-
-func (ct *consulRoutingTable) CacheGet(name string) (Pusher, bool) {
-	ptr := unsafe.Pointer(ct.cache)
-	tbl := atomic.LoadPointer(&ptr)
-	cp, ok := (*((*map[string]Pusher)(tbl)))[name]
-
-	return cp, ok
-}
-
-func (ct *consulRoutingTable) addEntry(tbl map[string]Pusher, val *consulValue) {
-	cp := &consulPusher{nil, string(val.Value)}
-
-	start := strings.Index(val.Key, "mailbox-routing/") + len("mailbox-routing/")
-
-	end := strings.LastIndex(val.Key, "/")
-
-	name := val.Key[start:end]
-
-	debugf("added to the consul route cache: %s => %s\n", name, string(val.Value))
-
-	cp.Connect()
-
-	if pusher, ok := tbl[name]; ok {
-		switch st := pusher.(type) {
-		case *consulPusher:
-			mp := NewMultiPusher()
-			mp.Add(st)
-			mp.Add(cp)
-
-			tbl[name] = mp
-		case *multiPusher:
-			st.Add(cp)
-		default:
-			panic("unknown pusher type?")
-		}
-	} else {
-		tbl[name] = cp
-	}
-}
-
-func (ct *consulRoutingTable) primeCache() (int, error) {
-	url := "http://localhost:8500/v1/kv/mailbox-routing/?recurse"
-
-	resp, err := http.Get(url)
-	if err != nil {
-		return 0, err
-	}
-
-	defer resp.Body.Close()
-
-	var values []consulValue
-
-	dec := json.NewDecoder(resp.Body)
-	err = dec.Decode(&values)
-	if err != nil && err != io.EOF {
-		return 0, err
-	}
-
-	tbl := make(map[string]Pusher)
-
-	for _, val := range values {
-		ct.addEntry(tbl, &val)
-	}
-
-	fmt.Printf("built table: %#v\n", tbl)
-
-	idx, _ := strconv.Atoi(resp.Header.Get("X-Consul-Index"))
-
-	cur := unsafe.Pointer(ct.cache)
-
-	atomic.StorePointer(&cur, unsafe.Pointer(&tbl))
-
-	return idx, nil
-}
-
-func (ct *consulRoutingTable) updateCache(idx int) {
-	url := "http://localhost:8500/v1/kv/mailbox-routing/?recurse"
-
-	for {
-		resp, err := http.Get(fmt.Sprintf("%s&index=%d&wait=5m", url, idx))
-		if err != nil {
-			time.Sleep(1 * time.Second)
-			continue
-		}
-
-		defer resp.Body.Close()
-
-		var values []consulValue
-
-		dec := json.NewDecoder(resp.Body)
-		err = dec.Decode(&values)
-		if err != nil && err != io.EOF {
-			time.Sleep(1 * time.Second)
-			continue
-		}
-
-		if len(values) == 0 {
-			continue
-		}
-
-		// Because consul does not send watches for deletes, when we detect
-		// a change, we have to reread all the routes.
-		ct.primeCache()
-
-		idx, _ = strconv.Atoi(resp.Header.Get("X-Consul-Index"))
-	}
 }
 
 func (ct *consulRoutingTable) Cleanup() error {
--- consul_routing_test.go
+++ consul_routing_test.go
@@ -122,3 +122,78 @@ func TestConsulRoutingTableWithMultipleDeclares(t *testing.T) {
 		t.Fatal("garbage addresses returned")
 	}
 }
+
+func TestConsulRoutingTableWithMultiPicksUpChanges(t *testing.T) {
+	m1 := NewMemRegistry()
+	m2 := NewMemRegistry()
+
+	ct1, err := NewConsulRoutingTable("127.0.0.1:8899")
+	if err != nil {
+		panic(err)
+	}
+
+	defer ct1.Cleanup()
+
+	ct2, err := NewConsulRoutingTable("127.0.0.1:9900")
+	if err != nil {
+		panic(err)
+	}
+
+	defer ct2.Cleanup()
+
+	ct3, err := NewConsulRoutingTable("127.0.0.1:9911")
+	if err != nil {
+		panic(err)
+	}
+
+	defer ct3.Cleanup()
+
+	ct1.Set("a", m1)
+	ct2.Set("a", m2)
+
+	// propogation delay.
+	time.Sleep(100 * time.Millisecond)
+
+	pusher, ok := ct3.Get("a")
+	if !ok {
+		t.Fatal("couldn't find a")
+	}
+
+	checked, ok := ct3.Get("a")
+	if !ok {
+		t.Fatal("couldn't find a")
+	}
+
+	if pusher != checked {
+		t.Fatal("pusher caching not working")
+	}
+
+	ct4, err := NewConsulRoutingTable("127.0.0.1:9922")
+	if err != nil {
+		panic(err)
+	}
+
+	defer ct4.Cleanup()
+
+	m3 := NewMemRegistry()
+
+	ct4.Set("a", m3)
+
+	// propogation delay.
+	time.Sleep(100 * time.Millisecond)
+
+	p2, ok := ct3.Get("a")
+
+	if p2 == pusher {
+		t.Fatal("cache did not update")
+	}
+
+	mp, ok := p2.(*multiPusher)
+	if !ok {
+		t.Fatal("multiPusher not returned")
+	}
+
+	if len(mp.pushers) != 3 {
+		t.Fatal("pusher doesn't have 3 servers")
+	}
+}
